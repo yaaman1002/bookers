7282128e797652a67e786977111405eb9557a591
yaaman1002 <166231007+yaaman1002@users.noreply.github.com>
2024-05-22 02:18:00 +0000

    「finish]edit,showページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,21 +6,44 @@ class BooksController < ApplicationController
   def create
     @book=Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book)
+       flash[:notice]="Book was successfully created."
+      redirect_to book_path(@book.id)
     else
      render :new
     end 
   end
   
   def index
-    @books=Book.all
+    @books=Book.new
   end
 
   def show
-     @book = Book.find(params[:id])
+     @book=Book.find(params[:id])
   end
 
   def edit
-     @book = Book.find(params[:id])
+     @book=Book.find(params[:id])
+  end
+  
+  def destroy
+    @book=Book.find(params[:id])
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to '/books'
+    end
+  end
+  
+  def update
+     book = Book.find(params[:id])
+     if book.update
+       flash[:notice]="Book was successfully updated."
+     redirect_to book_path(book.id)  
+     end
+  end
+  
+   private
+   
+  def book_params
+      params.require(:book).permit(:title, :body)
   end
 end
